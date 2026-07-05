49285a2f255cee6ba4320a69c632d6457d19827e
אמית דיגיטל <mopamit24@gmail.com>
2026-07-05 23:58:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,3 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  vite: {
-    base: '/-/',
-  },
-  tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
-    server: { entry: "server" },
-  },
-});
